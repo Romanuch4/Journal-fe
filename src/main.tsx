ffc69f63615aa25e635b8a994426d057ecdd11de
Romanuch4 <pomankrentovsky@gmail.com>
2026-03-08 15:29:47 +0200

    Add base CSP config

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import DOMPurify from "dompurify";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./app";
 
-if (window.trustedTypes && window.trustedTypes.createPolicy && DOMPurify) {
+if (window.trustedTypes && window.trustedTypes.createPolicy) {
   window.trustedTypes.createPolicy("default", {
-    createHTML: (string: string) => DOMPurify.sanitize(string),
+    createHTML: (string: string) => {
+      // we will cover it later
+      // return DOMPurify.sanitize(string)
+      return string;
+    },
     createScriptURL: (string: string) => string,
     createScript: (string: string) => string,
   });
